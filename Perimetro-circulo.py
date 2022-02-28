9f938895ffb25eb0fb51766ef84717d5abd37c43
FIRESLIM <91010497+Hgprogram06@users.noreply.github.com>
2022-02-28 12:03:06 +0100

    Create Perimetro-circulo.py

---
+++ Perimetro-circulo.py
@@ -0,0 +1,14 @@
+print("para calcular el perimetro introduce el radio")
+
+pi = 3.14
+a = 2
+
+multi = (a * pi)
+
+print("Introduce el radio")
+
+radio = int(input())
+
+perimetro = (multi * radio)
+
+print("El perimetro es igual a:", perimetro , "cm")
